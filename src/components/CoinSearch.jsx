877a466fc2102794f910466e40e7b82f0e5ff4d4
jaikangam <jaikangam12@gmail.com>
2022-06-10 01:13:34 +0530

    added trending component

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -7,7 +7,7 @@ const CoinSearch = ({coins}) => {
 
   const [searchText, setSearchText ] = useState('');
   return (
-    <div className='rounded=div my-4 pl-4'>
+    <div className='rounded-div my-4 pl-4'>
         <div className='flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right'>
             <h1 className='text-2xl font-bold my-2'> Search Crypto</h1>
             <form> 
@@ -15,19 +15,19 @@ const CoinSearch = ({coins}) => {
             </form>
         </div>
         <table className='w-full border-collapse text-center'>
-            <thead>
-                <tr className='border-b'>
-                    <th></th>
-                    <th className='px-4'>#</th>
-                    <th className='text-left'>Coin</th>
-                    <th></th>
-                    <th>Price</th>
-                    <th>24h</th>
-                    <th className='hidden md:table-cell'>24h Volume</th>
-                    <th className='hidden sm:table-cell'>Mkt</th>
-                    <th>Last 7 Days</th>
-                </tr>
-            </thead>
+        <thead>
+          <tr className='border-b'>
+            <th></th>
+            <th className='px-4'>#</th>
+            <th className='text-left'>Coin</th>
+            <th></th>
+            <th>Price</th>
+            <th>24h</th>
+            <th className='hidden md:table-cell'>24h Volume</th>
+            <th className='hidden sm:table-cell'>Mkt</th>
+            <th>Last 7 Days</th>
+          </tr>
+        </thead>
             <tbody>
                 {coins
                     .filter((value) => {
