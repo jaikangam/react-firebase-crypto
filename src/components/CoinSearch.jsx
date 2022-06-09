09d31d9f330993877c83315adfb5f1f58de9d239
jaikangam <jaikangam12@gmail.com>
2022-06-09 23:52:00 +0530

    Fixed the search bar

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -1,44 +1,46 @@
-import React from 'react';
-import { AiOutlineStar } from 'react-icons/ai';
+import React, { useState } from 'react';
+
+import CoinItem from './CoinItem';
 
 
 const CoinSearch = ({coins}) => {
 
+  const [searchText, setSearchText ] = useState('');
   return (
-    <div>
-        <div>
-            <h1> Search Crypto</h1>
+    <div className='rounded=div my-4 pl-4'>
+        <div className='flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right'>
+            <h1 className='text-2xl font-bold my-2'> Search Crypto</h1>
             <form> 
-                <input type="text" placeholder='Search a coin' />
+                <input className='w-full bg-primary border border-input px-4 py-2 rounded shadow-xl' onChange={(e) =>setSearchText(e.target.value) } type="text" placeholder='Search a coin' />
             </form>
         </div>
-        <table>
-            <thead> 
-                <tr>
-                    <th> </th>
-                    <th> # </th>
-                    <th> Coin </th>
-                    <th> </th>
-                    <th> Price </th>
-                    <th> 24th </th>
-                    <th> 24th Volume </th>
-                    <th> Mkt </th>
-                    <th> Last 7 Days </th>
-                
+        <table className='w-full border-collapse text-center'>
+            <thead>
+                <tr className='border-b'>
+                    <th></th>
+                    <th className='px-4'>#</th>
+                    <th className='text-left'>Coin</th>
+                    <th></th>
+                    <th>Price</th>
+                    <th>24h</th>
+                    <th className='hidden md:table-cell'>24h Volume</th>
+                    <th className='hidden sm:table-cell'>Mkt</th>
+                    <th>Last 7 Days</th>
                 </tr>
             </thead>
             <tbody>
-                {coins.map((coin) => (
-                    <tr> 
-                        <td> <AiOutlineStar /> </td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                {coins
+                    .filter((value) => {
+                    if (searchText === '') {
+                      return value;
+                    } else if (
+                      value.name.toLowerCase().includes(searchText.toLowerCase())
+                    ) {
+                      return value;
+                    }
+                  })
+                  .map((coin) => (
+                    <CoinItem key={coin.id} coin={coin}/>
                 ))}
             </tbody>
 
